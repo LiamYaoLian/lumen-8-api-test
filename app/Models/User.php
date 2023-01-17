c3dc2b24dc94031dfe6266b625335cf736af3f26
liamyaolian <liamyaolian@gmail.com>
2023-01-16 23:14:14 -0500

    clean up

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
-    use Authenticatable, Authorizable, HasFactory;
+    use Authenticatable, Authorizable;
 
     /**
      * The attributes that are mass assignable.
